2542bb7d746949568b8b661e6daf72d56ae8900a
xiangshouding <fansekey@gmail.com>
2015-07-13 14:52:12 +0800

    new

---
+++ index.js
@@ -0,0 +1,130 @@
+/**
+ * fis.baidu.com
+ */
+
+var path = require('path');
+
+module.exports = function(fis, mount) {
+  var sets = {
+    'namespace': '',
+    'static': 'static',
+    'template': 'template',
+    'smarty': {
+      'left_delimiter': '{%',
+      'right_delimiter': '%}'
+    }
+  };
+
+  var matchRules = {
+    // all release to $static dir 
+    '*': {
+      release: '/${static}/${namespace}/$0'
+    },
+    '*.{js,css,less}': {
+      useHash: true
+    },
+    '*.js': {
+      optimizer: fis.plugin('uglify-js')
+    },
+    '*.less': {
+      parser: fis.plugin('less'),
+      rExt: '.css'
+    },
+    '*.{css,less}': {
+      optimizer: fis.plugin('clean-css')
+    },
+    '::image': {
+      useHash: true
+    },
+    '*.png': {
+      optimizer: fis.plugin('png-compressor')
+    },
+    '*.tpl': {
+      preprocessor: fis.plugin('extlang'),
+      postprocessor: fis.plugin('require-async'),
+      optimizer: [
+        fis.plugin('smarty-xss'),
+        fis.plugin('html-compress')
+      ],
+      useMap: true,
+      release: '/${template}/${namespace}/$0'
+    },
+    '*.{tpl,js}': {
+      useSameNameRequire: true
+    },
+    // page dir
+    '/page/**.tpl': {
+      // 标记是否是个页面，向下兼容
+      extras: {
+        isPage: true
+      }
+    },
+    // widget
+    '/(widget/**.tpl)': {
+      url: '${namespace}/$1',
+      useMap: true,
+    },
+    '/widget/{*.{js,css},**/*.{js,css}}': {
+      isMod: true
+    },
+    '/{plugin,smarty.conf,domain.conf,**.php}': {
+      release: '$0'
+    },
+    'server.conf': {
+      release: '/server-conf/${namespace}.conf'
+    },
+    // test & config
+    '/({test,config})/(**)': {
+      useMap: false,
+      release: '/$1/${namespace}/$2'
+    },
+    '${namespace}-map.json': {
+      release: '/config/$0'
+    },
+    '::package': {
+      prepackager: [
+        fis.plugin('widget-inline'),
+        fis.plugin('js-i18n')
+      ]
+    }
+  };
+
+  function mount() {
+    // smarty
+    fis.set('system.localNPMFolder', path.join(__dirname, 'node_modules'));
+
+    fis.util.map(sets, function(key, value) {
+      fis.set(key, value);
+    });
+
+    fis.util.map(matchRules, function(selector, rules) {
+      fis.match(selector, rules);
+    });
+  
+    // 模块化支持
+    fis.hook('module', {
+      mode: 'commonJs'
+    });
+
+    // map.json
+    fis.match('::package', {
+      postpackager: function createMap(ret) {
+        var path = require('path')
+        var root = fis.project.getProjectPath();
+        var map = fis.file.wrap(path.join(root, fis.get('namespace') + '-map.json'));;
+        map.setContent(JSON.stringify(ret.map));
+        ret.pkg[map.subpath] = map;
+      }
+    });
+  }
+
+  if (mount !== false) {
+    mount();
+  }
+
+  return {
+    loadPath: path.join(__dirname, 'node_modules'),
+    sets: sets,
+    matchRules: matchRules
+  }
+};
